fce7b2f749260dbd092cf163e509e36cb5c9cde3
anka-kmbl <anna.maksymenko@nure.ua>
2017-10-27 11:54:58 +0300

    added interface switch

--- public/javascripts/chooseInterface.js
+++ public/javascripts/chooseInterface.js
@@ -1,18 +1,18 @@
-const interf1 = document.getElementById('interface1');
+// const interf1 = document.getElementById('interface1');
 
-interf1.onclick = (e) => {
-	return new Promise((res, rej) => {
-		const xhr = new XMLHttpRequest();
-		xhr.open('GET', '/api/interfaces/first');
-		xhr.send();
-		xhr.onload = (e) => {
-			const htmlXHR = e.srcElement;
-
-			if (htmlXHR.status !== 200) {
-				rej(htmlXHR.response);
-			}
-
-			res();
-		};
-	});
-}
+// interf1.onclick = (e) => {
+// 	return new Promise((res, rej) => {
+// 		const xhr = new XMLHttpRequest();
+// 		xhr.open('GET', '/api/interfaces/first');
+// 		xhr.send();
+// 		xhr.onload = (e) => {
+// 			const htmlXHR = e.srcElement;
+// 			console.log('loading');
+// 			if (htmlXHR.status !== 200) {
+// 				rej(htmlXHR.response);
+// 			}
+// 			console.log('almost resolved');
+// 			res();
+// 		};
+// 	});
+// }
