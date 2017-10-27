7bbe8686f698652afbe00fcff1c9b2eb75226ba9
sourmotion <he2ous47@gmail.com>
2017-10-27 11:27:05 +0300

    added buttonless convert

--- public/javascripts/decimal-binary.js
+++ public/javascripts/decimal-binary.js
@@ -1,4 +1,4 @@
-let num1 = document.getElementById('num1');
+ 	let num1 = document.getElementById('num1');
 let system2 = document.querySelectorAll('select')[0];
 let form = document.getElementById('form');
 
